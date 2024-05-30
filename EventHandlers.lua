d7b97c3610ccce2fd0b72acceb0d5de66ff1843a
David Mungin <david.mungin@gmail.com>
2024-05-29 20:57:07 -0400

    add support for non-masterloot loot history tracking

--- EventHandlers.lua
+++ EventHandlers.lua
@@ -3,11 +3,14 @@ local LootAlert = core.LootAlert;
 
 function LootAlert:UseLooterMessages(looterName)
     local lootMethod, _, masterlooterRaidID = GetLootMethod();
-
-    if lootMethod == 'master' and LootAlert.db.profile.showOnlyMaster then
-        local masterLooterName = GetRaidRosterInfo(masterlooterRaidID);
-        if masterLooterName ~= looterName then
-            return false;
+    if lootMethod ~= 'master' then
+        return false;
+    else
+        if LootAlert.db.profile.showOnlyMaster then
+            local masterLooterName = GetRaidRosterInfo(masterlooterRaidID);
+            if masterLooterName ~= looterName then
+                return false;
+            end
         end
     end
 
@@ -20,8 +23,17 @@ function LootAlert:HandleNewLoot(item)
         table.insert(LootAlert.db.char.lootHistory, 1, item.Id);
         LootAlert.db.char.lootHistoryLength = LootAlert.db.char.lootHistoryLength + 1;
 
+        local isWantedLoot = LootAlert.db.char.wantedLootBisList[item.Id] ~= nil;
+
         if LootAlert.state.tabFrame and LootAlert.db.char.activeTab == "lootHistory" then
             LootAlert:RenderLootHistory(LootAlert.state.tabFrame);
+        elseif isWantedLoot then
+            if not LootAlert.state.tabFrame then
+                LootAlert:RenderLootAlert();
+                LootAlert:SelectGroup('lootHistory');
+            elseif LootAlert.db.char.activeTab ~= "lootHistory" then
+                LootAlert:SelectGroup('lootHistory');
+            end
         end
     end
 end
@@ -55,10 +67,14 @@ function LootAlert:CHAT_MSG_RAID_WARNING(eventName, ...)
 end
 
 function LootAlert:START_LOOT_ROLL(eventName, ...)
-    local itemId = ...;
-    --local texture, name, count, quality = GetLootRollItemInfo(rollId);
-    if itemId then
-        LootAlert:GetItemInfo(tonumber(itemId), function (item)
+    local rollId = ...;
+    local itemLink = GetLootRollItemLink(rollId);
+
+    if itemLink then
+        local itemIdText = itemLink:match("item:(%d+):");
+        LootAlert:Debug("Started Loot Roll For : ", itemLink);
+        LootAlert:Debug("Getting Rolled Loot by ID : ", itemIdText);
+        LootAlert:GetItemInfo(tonumber(itemIdText), function (item)
             if item.Id ~= nil then
                 LootAlert:HandleNewLoot(item);
             end
